0af49a0c9354a4374bb7871df961d979e1baed1c
Chukwuebuka (Gaus Octavio ) <56232734+Chukwuebuka-2003@users.noreply.github.com>
2023-10-19 16:45:13 +0100

    the source code

---
+++ cgpa.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+def calculate_gpa(course_info):
+    total_points = 0
+    total_credits = 0
+
+    for course in course_info:
+        credits, grade = course['credits'], course['grade']
+        grade_points = get_grade_points(grade)
+        total_points += credits * grade_points
+        total_credits += credits
+
+    if total_credits == 0:
+        return 0
+
+    gpa = total_points / total_credits
+    return gpa
+
+def get_grade_points(grade):
+    grade_points = {
+        'A': 5, 'B': 4, 'C': 3, 'D': 2, 'E': 1, 'F': 0
+    }
+    return grade_points.get(grade, 0)
+
+def main():
+    num_semesters = int(st.number_input("Enter the desired number of semesters:"))
+    semester_gpas = []
+
+    for semester in range(1, num_semesters + 1):
+        st.header(f"Semester {semester}:")
+
+        num_courses = int(st.number_input(f"Enter the number of courses for Semester {semester}:"))
+        course_info = []
+
+        for i in range(num_courses):
+            course_name = st.text_input(f"Enter the name of course {i + 1}:")
+            credits = int(st.number_input(f"Enter the credit hours for course {i + 1}:"))
+            grade = st.text_input(f"Enter the grade for course {i + 1} (e.g., A, B, C, etc.):")
+            course_info.append({'name': course_name, 'credits': credits, 'grade': grade})
+
+        gpa = calculate_gpa(course_info)
+        st.success(f"Your GPA for Semester {semester} is: {gpa:.2f}")
+        semester_gpas.append(gpa)
+        st.write("\n")
+
+    cgpa = sum(semester_gpas) / len(semester_gpas)
+    st.success(f"Your Cumulative GPA (cGPA) for {num_semesters} semesters is: {cgpa:.2f}")
+
+if __name__ == "__main__":
+    main()
